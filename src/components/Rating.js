ede18c651514a872bb50874ff17c8504c890065d
sudharani <sudharani.ps01@gmail.com>
2025-07-10 18:44:44 +0530

    first commit

---
+++ src/components/Rating.js
@@ -0,0 +1,41 @@
+// Filename - components/Rating.js
+
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import { Container, Radio, Rating } from "./RatingStyles";
+const Rate = () => {
+    const [rate, setRate] = useState(0);
+    return (
+        <Container>
+            {[...Array(5)].map((item, index) => {
+                const givenRating = index + 1;
+                return (
+                    <label>
+                        <Radio
+                            type="radio"
+                            value={givenRating}
+                            onClick={() => {
+                                setRate(givenRating);
+                                alert(
+                                    `Are you sure you want to give 
+                                    ${givenRating} stars ?`
+                                );
+                            }}
+                        />
+                        <Rating>
+                            <FaStar
+                                color={
+                                    givenRating < rate || givenRating === rate
+                                        ? "#FFBF00"
+                                        : "grey"
+                                }
+                            />
+                        </Rating>
+                    </label>
+                );
+            })}
+        </Container>
+    );
+};
+
+export default Rate;
---
+++ src/components/RatingStyles.js
@@ -0,0 +1,18 @@
+// Filename - components/RatingStyles.js
+
+import styled from 'styled-components';
+
+export const Container = styled.div`
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   min-height: 0vh;
+   font-size: 25px;
+   `
+export const Radio = styled.input`
+   display: none;
+`
+export const Rating = styled.div`
+   cursor: pointer;
+   color: #BF4F74;
+`
