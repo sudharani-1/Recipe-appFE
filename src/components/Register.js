1cd26f93d300b8bf7b4acd9d27254a56d61f9de6
sudharani-1 <sudharani.ps01@gmail.com>
2025-07-10 19:53:51 +0530

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -20,7 +20,7 @@ const Register = () => {
 
     try {
       const response = await fetch(
-        "http://localhost:3001/auth/register",
+        "https://recipe-appbe.onrender.com/auth/register",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
